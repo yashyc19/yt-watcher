dadda01c555e6ff9cc93cfdceee4a3291e17faa6
yashyc19 <yashyc19@gmail.com>
2024-10-11 03:27:00 +0530

    Status: initial commit proper

--- image-server-REST/users/README.md
+++
@@ -1,9 +0,0 @@
-# User APIs
-
-## user.service
-Module that has all the methods that are going to be executed from the **user.controller** module
-
-These functions interact with the backend data that are accessible to the users
-
-## user.controller
-User's API handler that will run **user.services** functions depending on which API is called on the frontend
--- valorant/lotus/__init__.py
+++
@@ -1,9 +0,0 @@
-# # Build the project
-# 1. Run `poetry build` in the project directory
-
-# # Install the project in the production environment
-# 2. Transfer the generated .whl file to the production environment
-# 3. In the production environment, run `pip install dist/<your_project>.whl`
-
-# # Run the project
-# 4. Run the commands `test-main` and `demo` directly in the command line
